0ad2ff18e6a954851bf81503edf78fcdf1914edc
user-unix <nair.2@iitj.ac.in>
2024-03-30 01:03:42 +0530

    merged withh feature-2 and bug-fix

--- file-feature-1.py
+++ file-feature-1.py
@@ -3,4 +3,10 @@ import numpy as np
 
 random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 
+<<<<<<< HEAD
 acts.activation(np.array(random_values),'sigmoid','print')
+=======
+for activ in ['sigmoid','relu','leakyrelu','tanh'] :
+    print(f'Activation = {activ}')
+    acts.activation(np.array(random_values),activ,'print') 
+>>>>>>> feature-2
